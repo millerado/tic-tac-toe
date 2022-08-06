d2c1563caf2785c63c6ed02f782675b71f0daf3e
Seth Miller <millerado@gmail.com>
2022-08-05 19:40:25 -0600

    init game variables

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ for (let i = 0; i < 9; i++) {
   boardEl.appendChild(box);
   box.setAttribute("id", `box-${i}`);
 }
+
+// Assign game tiles to variables
+const box0 = document.getElementById("box-0");
+const box1 = document.getElementById("box-1");
+const box2 = document.getElementById("box-2");
+const box3 = document.getElementById("box-3");
+const box4 = document.getElementById("box-4");
+const box5 = document.getElementById("box-5");
+const box6 = document.getElementById("box-6");
+const box7 = document.getElementById("box-7");
+const box8 = document.getElementById("box-8");
+
+// Define game state variables
+const boardArray = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+let playerTurn = 1; // 1 = player1, -1 = player2
+let winner = "playing"; //playing, tied, player that won
