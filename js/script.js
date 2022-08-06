ef10783216b94c2824823bc415ec76b3d6cd33da
Seth Miller <millerado@gmail.com>
2022-08-05 19:13:29 -0600

    gave boardEl and ID

--- js/script.js
+++ js/script.js
@@ -5,12 +5,11 @@ const mainEl = document.querySelector("main");
 // create the board element
 const boardEl = document.createElement("div");
 mainEl.appendChild(boardEl);
+boardEl.setAttribute("id", "board");
 
-// create the 9 elements which will be the game titles
+// create the 9 elements which will be the game tiles
 for (let i = 0; i < 9; i++) {
   const box = document.createElement("div");
   box.setAttribute("class", "game-space");
   boardEl.appendChild(box);
 }
-
-console.log(boardEl);
