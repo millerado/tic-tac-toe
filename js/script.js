cffa9ea1b7cd1bb1b060bf19e83ce603458020d0
Seth Miller <millerado@gmail.com>
2022-08-05 19:05:51 -0600

    create board and games tiles using DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// Creating the elements using the DOM
 
+const mainEl = document.querySelector("main");
+
+// create the board element
+const boardEl = document.createElement("div");
+mainEl.appendChild(boardEl);
+
+// create the 9 elements which will be the game titles
+for (let i = 0; i < 9; i++) {
+  const box = document.createElement("div");
+  box.setAttribute("class", "game-space");
+  boardEl.appendChild(box);
+}
+
+console.log(boardEl);
